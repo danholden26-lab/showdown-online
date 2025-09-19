6b94a4e6258bccd5b7e84ec0f85fb61dd5b25cd7
danholden26-lab <dan.holden26@gmail.com>
2025-09-19 16:28:33 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '../ShowdownGame.jsx';
+import App from '../showdownGame.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
